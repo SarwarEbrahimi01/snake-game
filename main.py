23aa6a02f8b0ec64f8e37cb8e481ae9dbcc47212
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-03-03 09:18:24 +0430

    Update main.py
    Keep track of the snake food and the game score

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-# In order to develop this game we need to divide the problem into 7 separate tasks/Steps
-from turtle import Screen, Turtle
+# In order to develop this game we need to divide the problem into 7 seperate tasks/Steps
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -21,7 +24,21 @@ screen.tracer(0)
 # Step 4:
 # Control the snake
 
+# Step 5:
+# Detect Collisions with Food
+
+# Step 6:
+# Creating a Scoreboard
+
+# Step 7:
+# Detect Collision with wall
+
+# Step 8:
+# Detect Collision with Tail
+
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,11 +46,39 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
     snake.move()
+
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect Collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+    
+    # Detect Collision with Tail
+    #Using python slicing
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    
+
+    
+  
+
+
+    
+       
 
 screen.exitonclick()
