85c2f8eb1658caff72bf6fa31b24343db24ecb9b
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-03-07 10:13:41 +0430

    Update snake.py
    The high score functionality

--- snake.py
+++ snake.py
@@ -1,10 +1,11 @@
 from turtle import Turtle
-STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
 
 class Snake:
 
@@ -12,13 +13,10 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-    
+
     def create_snake(self):
-        for position in STARTING_POSITION:
+        for position in STARTING_POSITIONS:
             self.add_segment(position)
-            
-    def extend(self):
-        self.add_segment(self.segments[-1].position())
 
     def add_segment(self, position):
         new_segment = Turtle("square")
@@ -27,19 +25,28 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-
         self.head.forward(MOVE_DISTANCE)
-     
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
-    
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
@@ -47,7 +54,7 @@ class Snake:
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
-    
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
